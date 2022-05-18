73687b5ccec80ce3c74858ca4c15b5cec09ff6cc
rihanmalikbadagan <91051471+rihanmalikbadagan@users.noreply.github.com>
2022-05-18 07:31:54 +0200

    Update stats.c
    Initial commit

--- stats.c
+++ stats.c
@@ -5,6 +5,25 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    int sum = 0;
+    for (int i = 0; i < setlength; i++)
+    {
+        if (numberset[i] < s.min)
+        {
+            s.min = numberset[i];   
+        }
+        
+        if (numberset[i] > s.max)
+        {
+            s.max = numberset[i];   
+        }
+        
+        sum = sum + numberset[i];
+    }
+    
+    s.average = sum / setlength;
+    
+    return s;
 }
 
 int emailAlertCallCount = 0;
