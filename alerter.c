efcfe8ea1c3636bb31894b1c054122df1175169a
Rihan Malik <rihannb@yahoo.in>
2022-05-19 13:16:06 +0200

    Implementing alert functions

---
+++ alerter.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+#include "stats.h"
+
+void check_and_alert(const float maxThreshold, alerter_funcptr alerters[], struct Stats s)
+{
+    int status = FALSE;
+    if (s.max > maxThreshold)
+    {
+        status = TRUE;
+    } else
+    {
+        status = FALSE;
+    }
+    alerters [0] = emailAlerter(status);
+    alerters [1] = ledAlerter(status);
+}
+
+void emailAlerter(int status)
+{
+    if (status == TRUE)
+    {
+        emailAlertCallCount = 1;
+    }
+}
+
+void ledAlerter(int status)
+{
+    if (status == TRUE)
+    {
+        ledAlertCallCount = 1;
+    }
+}
--- stats.h
+++ stats.h
@@ -1,8 +1,16 @@
+struct Stats
+{
+    average;
+    min;
+    max;
+};
 
 struct Stats compute_statistics(const float* numberset, int setlength);
 
-typedef void (*alerter_funcptr)();
+typedef void (*alerter_funcptr)(int);
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+void emailAlerter(int status);
+void ledAlerter(int status);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
